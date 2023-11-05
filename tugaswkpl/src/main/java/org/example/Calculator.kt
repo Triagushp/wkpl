d30aee350df8430517a8c997dcf1b587e1030603
Triagushp <124849560+Triagushp@users.noreply.github.com>
2023-11-05 12:05:30 +0700

    second commit Calculator.kt

--- tugaswkpl/src/main/java/org/example/Calculator.kt
+++ tugaswkpl/src/main/java/org/example/Calculator.kt
@@ -1,11 +1,12 @@
-package org.example
 
-class Calculator {
-    fun add(first: Int, second: Int): Int {
-        return first + second
+package org.example // Mendefinisikan paket tempat kelas Calculator berada
+
+class Calculator { // Mendefinisikan kelas Calculator
+    fun add(first: Int, second: Int): Int { // Fungsi penambahan, menerima dua parameter dan mengembalikan hasil penjumlahan
+        return first + second // Mengembalikan hasil penjumlahan dari dua parameter
     }
 
-    fun sub(first: Int, second: Int): Int {
-        return first - second
+    fun sub(first: Int, second: Int): Int { // Fungsi pengurangan, menerima dua parameter dan mengembalikan hasil pengurangan
+        return first - second // Mengembalikan hasil pengurangan dari dua parameter
     }
 }
