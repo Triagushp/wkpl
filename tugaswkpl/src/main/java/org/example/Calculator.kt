d58d2c129aedf64e28a879856dd03722e4421652
Triagushp <124849560+Triagushp@users.noreply.github.com>
2023-11-05 12:03:31 +0700

    first commit

---
+++ tugaswkpl/src/main/java/org/example/Calculator.kt
@@ -0,0 +1,11 @@
+package org.example
+
+class Calculator {
+    fun add(first: Int, second: Int): Int {
+        return first + second
+    }
+
+    fun sub(first: Int, second: Int): Int {
+        return first - second
+    }
+}
